35049b990a649e5f5ac61c867183c8fb16a7f36d
j32u4ukh <j32u4ukh@gmail.com>
2019-09-20 23:10:21 +0800

    完成 index addtion 但尚未幫不同字對應到不同 addtion

--- Step1_00000000/enigma.py
+++ Step1_00000000/enigma.py
@@ -10,8 +10,7 @@ class Pipeline:
     def __init__(self):
         """初始字元順序"""
         self.origin_sequence = []
-        self.index_to_char = {}
-        self.char_to_index = {}
+        self.length = 0
         
     def defaultSequence(self):
         for i in range(10):
@@ -33,11 +32,38 @@ class Pipeline:
         else:
             for _item in _items:
                 self.addOriginItem(_item)
+        
+        # 保存長度資訊
+        self.length = len(self.origin_sequence)
             
     def addOriginItem(self, _item):
         """將 _item 的值放入 self.origin_sequence"""
-        assert type(_item) is chr, "_item 的類型必須是 chr"
+        assert (type(_item) is chr) or  (type(_item) is str), "_item 的類型必須是 chr 或 str"
         self.origin_sequence.append(_item)
+        
+    def getIndexAddition(self, _temp_array):
+        _addition = []
+        _length = len(_temp_array)
+        for i in range(_length):
+            _curr = _temp_array[i]
+            _curr_index = self.getIndex(_curr)
+            
+            _next = i + 1 
+            
+            if _next == _length:
+                _next = 0
+                
+            _next_char = _temp_array[_next]
+            _next_index = self.getIndex(_next_char)
+            
+            _index_gap = _next_index - _curr_index
+            
+            if _index_gap < 0:
+                _index_gap += _length
+                
+            _addition.append(_index_gap)
+            
+        return _addition        
         
     def getIndex(self, _char):
         for _index, _item in enumerate(self.origin_sequence):
@@ -61,13 +87,23 @@ class Reflector(Pipeline):
     def __init__(self, _items=None):
         super().__init__()
         self.addOriginItems(_items)
-        self.reflector_sequence = self.origin_sequence.copy()
+        self.addtion = None
+        self.setReflector()
+        self.pointer = 0
         
     def setReflector(self):
         _temp_sequence = self.origin_sequence.copy()
         random.shuffle(_temp_sequence)
-        print(_temp_sequence)
+        self.addtion = self.getIndexAddition(_temp_sequence)
         
+    def transform(self, _input):
+        if type(_input) is str:
+            _input = self.getIndex(_input)
+        
+        _output = _input + self.addtion[self.pointer]
+        _output = _output % self.length
+        
+        return self.origin_sequence[_output]
 
 class Rotor(Pipeline):
     def __init__(self, _carry):
@@ -83,9 +119,14 @@ class Enigma:
         print("Hello Enigma!")
 
 
-if __name__ == "__main__":        
-    reflector = Reflector()
+if __name__ == "__main__":   
+    string = "Hello World"
+    items = list(set(list(string)))
+    
+    reflector = Reflector(items)
     print("origin")
     print(reflector.origin_sequence)
     print("reflector")
-    reflector.setReflector()
+    print(reflector.addtion)
+    for i in string:
+        print(i, reflector.transform(i))
--- Step1_00000000/test.py
+++ Step1_00000000/test.py
@@ -4,6 +4,7 @@ Created on Wed Sep 18 21:57:00 2019
 
 @author: j32u4ukh
 """
+import numpy as np
 
 def numberASCII():
     # 48 ~ 57
@@ -27,14 +28,76 @@ def alphabetASCII():
     for i in range(97, 123):
         char = chr(i)
         print(i, char)
-        
+
+      
 def sixQuotationMarksTest():
     print("""a {} 
     b {} 
     c {}""".format(1,
     2, 
     3))
+    
 
-numberASCII()        
-# alphabetASCII()
-#sixQuotationMarksTest()
+def chineseTest():
+    # print(ord('ˊ'))  # 714
+    # print(ord('ˇ'))  # 711
+    # print(ord('ˋ'))  # 715
+    # print(ord('˙'))  # 729
+    # print(ord('ㄅ'))  # 12549
+    # print(ord('ㄦ'))  # 12582
+    # print(ord('ㄧ'))  # 12583
+    # print(ord('ㄨ'))  # 12584
+    # print(ord('ㄩ'))  # 12585
+    
+    for i in range(12549, 12582 + 1):
+        print(i, chr(i))
+        
+    
+def japaneseTest():
+    japanese = ['あ', 'い', 'う', 'え', 'お',
+                'か', 'き', 'く', 'け', 'こ',
+                'さ', 'し', 'す', 'せ', 'そ',
+                'ら', 'り', 'る', 'れ', 'ろ',]
+    
+    for j in japanese:
+        print(j, ord(j))
+        
+    print(chr(12354 + 3))
+
+if __name__ == "__main__":
+    # numberASCII()        
+    # alphabetASCII()
+    # sixQuotationMarksTest()
+    # chineseTest()
+    # japaneseTest()
+    def getPointer(_array, _char):
+        for i, c in enumerate(_array):
+            if c == _char:
+                return i
+            
+    origin = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    shuffle = ['1', '2', '8', '9', '4', '0', '5', '3', '6', '7']
+    pointer = []
+    length = len(shuffle)
+    for index in range(length):
+        curr = shuffle[index]
+        curr_pointer = getPointer(origin, curr)
+        
+        next_index = index + 1 
+        
+        if next_index == length:
+            next_index = 0
+            
+        next_char = shuffle[next_index]
+        next_pointer = getPointer(origin, next_char)
+        
+        pointer_plus = next_pointer - curr_pointer
+        
+        if pointer_plus < 0:
+            pointer_plus += length
+            
+        pointer.append(pointer_plus)
+        
+    print(pointer)
+        
+        
